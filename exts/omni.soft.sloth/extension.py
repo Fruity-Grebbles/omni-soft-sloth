df4ee1e2aba16c1ba6ab5b7cdf0da115c4e2a3e6
Fruity-Grebbles <jhg29@njit.edu>
2023-03-29 19:34:54 -0400

    docs, params

--- exts/omni.soft.sloth/extension.py
+++ exts/omni.soft.sloth/extension.py
@@ -15,6 +15,7 @@ class Extension(omni.ext.IExt):
     def on_shutdown(self):
         self.should_continue = False
         if self.udp_socket:
+            # shutdown the socket if it exists
             print("shutdown socket")
             self.udp_socket.shutdown(socket.SHUT_RDWR)
             # wait for the thread to finish
@@ -23,13 +24,29 @@ class Extension(omni.ext.IExt):
                 time.sleep(0.1)
             self.udp_socket = None
 
-    def udp(self):
+    def udp(self, ip : str, port: int, max_attempts: int = 10):
+        """
+        Creates a UDP socket and listens for incoming messages. We will run it
+        in a thread so it doesn't block the main thread.
+
+        Parameters
+        ----------
+        ip : str
+            The IP address to listen on.
+        port : int
+            The port to listen on.
+        max_attempts : int, optional
+            The maximum number of attempts to connect to the server. The default is 10.
+        """
+
+        # Create a datagram (UDP) socket
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        max_attempts = 10
+
+        # Attempt to bind to ip and port. Will retry for max_attempts
         for i in range(max_attempts):
             print("attempting to connect to server:", i)
             try:
-                self.udp_socket.bind(("127.0.0.1", 20001)) 
+                self.udp_socket.bind((ip, port)) 
                 break
             except:
                 time.sleep(1)
@@ -37,18 +54,25 @@ class Extension(omni.ext.IExt):
                 print("failed to connect to server")
                 self.udp_socket = None
                 return
+        # Listen for incoming datagrams
         self.udp_socket.listen(1)
 
         print("connecting...")
+
+        # Accept a connection
         try:
             self.client_socket, self.ip_port = self.udp_socket.accept()
+        # If we get an error, print it and close the socket
         except Exception as e:
             print(e)
             return
+
+        # Print the client's IP address
         print("client", self.ip_port, "connected")
         
         self.running = True
         self.should_continue = True
+
         while self.should_continue:
             bytesAddressPair = self.udp_socket.recvfrom(1024)
 
