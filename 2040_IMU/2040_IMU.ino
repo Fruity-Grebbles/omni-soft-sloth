33109bb3ce0f4130eefbfdd3919c1562ead0a7fe
Fruity-Grebbles <jgreb314@gmail.com>
2023-04-06 16:29:27 -0400

    Update 2040_IMU.ino
    switch to wpa2 standard

--- 2040_IMU/2040_IMU.ino
+++ 2040_IMU/2040_IMU.ino
@@ -59,8 +59,8 @@ void setup() {
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.beginEnterprise(ssid, user, pass);
+    // Connect to WPA/WPA2 network. Change this line if using open, enterprise or WEP network:
+    status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
     delay(10000);
